dcbce7a90fefc6dad8afa4e00c462574c1ea16f5
DaeWon <qkreodnjs97@naver.com>
2022-05-08 20:27:14 +0900

    [ADD] MainFrame에 배경이미지 추가

--- ImageSearch/src/View/MainFrame.java
+++ ImageSearch/src/View/MainFrame.java
@@ -1,7 +1,9 @@
 package View;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import Utility.Constant;
@@ -13,10 +15,10 @@ public class MainFrame extends JFrame
 		setTitle(Constant.WINDOW_TITLE);
 		setSize(Constant.WINDOW_WIDTH, Constant.WINDOW_HEIGHT);
 		setLayout(null);
-		
+	
 		JTextField inputTextFiled = new JTextField();
-		inputTextFiled.setSize(500,30);
-		inputTextFiled.setLocation(150,200);
+		inputTextFiled.setSize(300,30);
+		inputTextFiled.setLocation(250,200);
 		add(inputTextFiled);
 		
 		JButton searchButton = new JButton("검색하기");
@@ -28,6 +30,11 @@ public class MainFrame extends JFrame
 		showLogButton.setSize(90,20);
 		showLogButton.setLocation(400, 250);
 		add(showLogButton);
+		
+		ImageIcon imageIcon = new ImageIcon(MainFrame.class.getResource("/Image/MainBackground.jpg"));
+		JLabel backgroundImage = new JLabel(imageIcon);
+		backgroundImage.setBounds(0,0,800,600);
+		add(backgroundImage);
 	}
 	
 	public void Show()
