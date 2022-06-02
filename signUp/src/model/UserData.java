8ec35cd0e13fd122128d6d6ff87b5d6fed0fa90e
DaeWon <qkreodnjs97@naver.com>
2022-06-02 22:47:19 +0900

    [ADD] Constant, UserData 클래스 생성

--- signUp/src/model/UserData.java
+++ signUp/src/model/UserData.java
@@ -1,5 +1,18 @@
 package model;
 
-public class UserData {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import utility.Constant;
+
+
+public class UserData 
+{
+	private Connection connection;
+	private Connection getConnection() throws ClassNotFoundException, SQLException
+	{
+		Connection connection = DriverManager.getConnection(Constant.DATABASE_CONNECTION_INFORMATION, Constant.DATABASE_USER_NAME, Constant.DATABASE_PASSWORD);
+		return connection;
+	}
 }
---
+++ signUp/src/utility/Constant.java
@@ -0,0 +1,8 @@
+package utility;
+
+public class Constant 
+{
+	public static final String DATABASE_CONNECTION_INFORMATION = "jdbc:mysql://localhost:3307/daewonSignUp";
+	public static final String DATABASE_USER_NAME = "root";
+	public static final String DATABASE_PASSWORD = "0000";
+}
